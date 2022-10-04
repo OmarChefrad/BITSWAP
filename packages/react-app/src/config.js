b78b4e68cd2876cd81cead3117c4a154c56b7072
Omar <omarkiojoo@gmail.com>
2022-10-04 14:52:13 +0100

    Wallet

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -3,8 +3,9 @@ import { Goerli } from "@usedapp/core";
 export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
 
 export const DAPP_CONFIG = {
-  readOnlyChainId: Goerli.chainId,
-  readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
-  },
-};
+         readOnlyChainId: Goerli.chainId,
+         readOnlyUrls: {
+           [Goerli.chainId]:
+             "https://eth-goerli.g.alchemy.com/v2/_rqZwvGEhbL1lcJuUEdARCxWKxl1iA7t",
+         },
+       }
